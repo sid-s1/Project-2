ff23530c31bb881d21f831dd54fc1d5138813268
Siddharth Singh <siddharth.s1@outlook.com>
2022-07-23 10:31:37 +1000

    Added database; added python functionality to request from google api and get info regarding the place that has been searched for

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, render_template
+import requests, os, psycopg2
+
+DATABASE_URL = os.environ.get('DATABASE_URL','dbname=shopping_list')
+key_from_file = open('sec_key.txt','r').read()
+SECRET_KEY = os.environ.get('SECRET_KEY',key_from_file)
 
 app = Flask('__name__')
-
+app.secret_key = SECRET_KEY.encode()
+api_key = open('maps.txt','r').read()
 
 @app.route('/')
 def index():
@@ -9,7 +15,23 @@ def index():
 
 @app.route('/list')
 def shopping_list():
-    return render_template('shopping.html')
+    return render_template('shopping.html',key=api_key)
+
+@app.route('/list',methods=["POST"])
+def shopping_list_search():
+    searched_store = request.form.get('searched-store')
+    url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={searched_store}&types=supermarket&location=-33.814999%2C151.001114&radius=10000&strictbounds=true&key={api_key}"
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    # use places details api google and use place_id to show some details to user in html so they can choose, then once they choose through another form perhaps make it pin drop on maps with route from their home
+    print(response.text)
+    return render_template('shopping.html',key=api_key)
+
+
+@app.route('/display')
+def list_route_display():
+    return render_template('route.html',key=api_key)
 
 
 if __name__ == '__main__':
--- static/shopping.js
+++ static/shopping.js
@@ -3,6 +3,10 @@ const cancelAddStore = document.querySelector('#cancel-add-store');
 const storeSearch = document.querySelector('#store-search');
 const storeFlexBox = document.querySelector('#store-flexbox');
 const itemFlexBox = document.querySelector('#item-flexbox');
+const searchForm = document.querySelector('search-form');
+const searchField = document.querySelector('#search-store-field');
+const searchButton = document.querySelector('#search-button');
+let autoComplete;
 
 cancelAddStore.style.display = 'none';
 storeSearch.style.display = 'none';
@@ -13,21 +17,31 @@ cancelAddStore.addEventListener('click', function () {
     addStore.style.display = 'block';
 });
 
-addStore.addEventListener('click', function () {
+function addStoreFunc() {
     addStore.style.display = 'none';
     storeSearch.style.display = 'flex';
     cancelAddStore.style.display = 'block';
+}
 
-    const searchForm = document.createElement('form');
-    const searchField = document.createElement('input');
-    const searchButton = document.createElement('button');
-    searchField.classList.add('bordered-input-box');
-    searchButton.textContent = 'Search Store';
-    searchButton.classList.add('centered-btn');
-    searchButton.setAttribute('id', 'form-btn');
+addStore.addEventListener('click', addStoreFunc);
 
-    searchForm.classList.add('flex-for-col');
-    searchForm.append(searchField, searchButton);
-    storeFlexBox.append(searchForm);
+function initMap() {
+    autoComplete = new google.maps.places.Autocomplete(searchField, {
+        types: ['establishment'],
+        componentRestrictions: { 'country': ['AU'] },
+        fields: ['place_id', 'geometry', 'name']
+    });
+    autoComplete.addListener('place_changed', onPlaceChanged);
+}
 
-});
+function onPlaceChanged() {
+    const place = autoComplete.getPlace();
+    if (!place.geometry) {
+        searchField.placeholder = "Enter a place";
+    }
+    else {
+        const p = document.createElement('p');
+        p.textContent = place.name;
+        document.body.appendChild(p);
+    }
+}
